55992967371ac7a2e64d14ca18ca16afc3a6ef99
luka rocha <35671566+lukarmar@users.noreply.github.com>
2023-01-05 16:02:28 -0300

    fix: change next config (#3)

--- next.config.js
+++ next.config.js
@@ -1,23 +1,8 @@
-const pwa = require('next-pwa');
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
+
+module.exports = {
   images: {
     domains: ['i.dummyjson.com'],
-  },
-  compiler: {
-    removeConsole: process.env.NODE_ENV !== 'development',
-  },
-  pwa: {
-    dest: 'public',
-    disable: process.env.NODE_ENV === 'development',
-    register: true,
-  },
-};
-
-module.exports = (_phase, { defaultConfig }) => {
-  const plugins = [pwa]
-  return plugins.reduce((acc, plugin) => plugin(acc), { ...nextConfig })
+  }
 }
